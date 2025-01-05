c0e1a1d4c12be9a582a7a59d2246845fe7ea5f3a
Aleksandr Trushkin <a-trushkin@yandex-team.ru>
2025-01-05 23:18:02 +0700

    add humble test

--- zerolog_test.go
+++ zerolog_test.go
@@ -1,8 +1,11 @@
 package xlog
 
 import (
+	"context"
+	"io"
 	"log/slog"
 	"os"
+	"testing"
 
 	"github.com/rs/zerolog"
 )
@@ -20,4 +23,39 @@ func ExampleNewZerologHandler() {
 		With(slog.Int("group_id", 3)).
 		Warn("run", slog.String("who", "forest"))
 	// Output: {"level":"warn","id":1,"bro":{"bro_id":2,"group_1":{"group_id":3,"who":"forest"}},"message":"run"}
+}
+
+func TestZerologHandlerEnabled(t *testing.T) {
+	levels := []zerolog.Level{
+		zerolog.DebugLevel,
+		zerolog.InfoLevel,
+		zerolog.WarnLevel,
+		zerolog.ErrorLevel,
+	}
+
+	sLevels := []slog.Level{
+		slog.LevelDebug,
+		slog.LevelInfo,
+		slog.LevelWarn,
+		slog.LevelError,
+	}
+
+	availableLevels := make(map[slog.Level]struct{}, len(levels))
+	for _, level := range sLevels {
+		availableLevels[level] = struct{}{}
+	}
+
+	ctx := context.Background()
+	for i, sLevel := range sLevels {
+		zLevel := levels[i]
+
+		log := zerolog.New(io.Discard).Level(zLevel)
+		handler := NewZerologHandler(log, nil)
+
+		_, enabled := availableLevels[sLevel]
+		if handler.Enabled(ctx, sLevel) != enabled {
+			t.Errorf("level should be enabled: %t but got opposite", enabled)
+		}
+		delete(availableLevels, sLevel)
+	}
 }
